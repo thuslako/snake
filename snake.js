94ee567de3f6914b8e76091b8c522702e3ac2451
Luk <lukundu.lako@gmail.com>
2022-12-28 00:32:19 -0500

    update: resolved sorting and fetching position bug

--- snake.js
+++ snake.js
@@ -1,11 +1,12 @@
 export function setupGame() {
   const boxSize = 20;
-  const row = 25;
-  const col = 25;
-  const snake = [
+  const rows = 25;
+  const cols = 25;
+  let snake = [
     [0, 0],
     [0, 1],
     [0, 2],
+    [0, 3],
   ];
 
   const keys = {
@@ -13,6 +14,10 @@ export function setupGame() {
     RIGHT: 39,
     UP: 38,
     DOWN: 40,
+    A: 65,
+    D: 68,
+    W: 87,
+    S: 83,
   };
 
   const boxes = new Map();
@@ -27,26 +32,33 @@ export function setupGame() {
   let board = document.getElementById("board");
   setInterval(() => update(), 100);
 
-  const snakeSections = (snake) =>
-    snake.map((section) => `${section[0]}${section[1]}`);
+  const snakeSections = (snake) => snake.map(([i, j]) => `${i}_${j}`);
 
   const input = (e) => {
+    const lastMove = moveQueue[moveQueue.length - 1];
     switch (e.keyCode) {
       case keys.LEFT:
-        moveQueue.push("LEFT");
+      case keys.A:
+        if (lastMove !== "RIGHT") moveQueue.push("LEFT");
         break;
       case keys.RIGHT:
-        moveQueue.push("RIGHT");
+      case keys.D:
+        if (lastMove !== "LEFT") moveQueue.push("RIGHT");
         break;
       case keys.UP:
-        moveQueue.push("UP");
+      case keys.W:
+        if (lastMove !== "DOWN") moveQueue.push("UP");
         break;
       case keys.DOWN:
-        moveQueue.push("DOWN");
+      case keys.S:
+        if (lastMove !== "UP") moveQueue.push("DOWN");
+        break;
       default:
+        moveQueue.push("RIGHT");
         break;
     }
-    // console.log(moveQueue);
+    console.log(moveQueue);
+    console.log(snake);
   };
 
   window.addEventListener("keydown", input);
@@ -56,7 +68,9 @@ export function setupGame() {
     snake.shift();
     let head = snake[snake.length - 1];
     const _move = moves[moveQueue.pop()];
-    snake.push(_move(head));
+    const _head = _move(head);
+    console.log(snake);
+    snake.push(_head);
   };
 
   const update = () => {
@@ -67,33 +81,29 @@ export function setupGame() {
   //draws current snake based on snake values
   const addSnake = (snake) => {
     let _snake = snakeSections(snake);
-    let box = null;
-    for (let i = 0; i < row; i++) {
-      for (let j = 0; j < col; j++) {
-        _snake.forEach((section) => {
-          if (boxes.has(section)) {
-            box = boxes.get(section);
-            box.style.background = "#A3CB38";
-          }
-        });
-        box = boxes.get(`${i}${j}`);
-        box.style.background = "#222f3e";
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        let position = `${i}_${j}`;
+        let box = boxes.get(position);
+        box.style.background =
+          _snake.indexOf(position) >= 0 ? "#A3CB38" : "#222f3e";
       }
     }
   };
   const initCanvas = () => {
-    for (let i = 0; i < row; i++) {
-      for (let j = 0; j < col; j++) {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
         let box = document.createElement("div");
         box.style.position = "absolute";
         box.style.width = `${boxSize}px`;
         box.style.height = `${boxSize}px`;
         box.style.border = "1px solid rgba(235, 255, 114,0.4)";
+        box.style.borderRadius = "2px";
         box.style.background = "#222f3e";
         box.style.top = `${i * boxSize}px`;
         box.style.left = `${j * boxSize}px`;
         board.appendChild(box);
-        boxes.set(`${i}${j}`, box);
+        boxes.set(`${i}_${j}`, box);
       }
     }
     addSnake(snake);
